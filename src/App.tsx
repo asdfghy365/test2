946281dfdf3723b21823b7d1a1213a319841a448
Meme <arman932@gmail.com>
2024-06-09 20:28:59 +0300

    Your commit message describing the changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css';
-import '@twa-dev/sdk';
 import { TonConnectButton } from '@tonconnect/ui-react';
-import { useCounterContract } from './hooks/useCounterContract.ts';
+import { useTonConnect } from './hooks/useTonConnect.ts';
+import { useCounterContract } from './hooks/useCounterContract';
 
 function App() {
-  const { value, address } = useCounterContract();
+  const { connected } = useTonConnect();
+  const { value, address, sendIncrement } = useCounterContract();
 
   return (
     <div className='App'>
@@ -20,6 +21,15 @@ function App() {
           <b>Counter Value</b>
           <div>{value ?? 'Loading...'}</div>
         </div>
+
+        <a
+          className={`Button ${connected ? 'Active' : 'Disabled'}`}
+          onClick={() => {
+            sendIncrement();
+          }}
+        >
+          Increment
+        </a>
       </div>
     </div>
   );
--- src/hooks/useCounterContract.ts
+++ src/hooks/useCounterContract.ts
@@ -1,17 +1,21 @@
 import { useEffect, useState } from 'react';
-import Counter from '../contracts/Counter';
+import Counter from '../contracts/Counter.ts';
 import { useTonClient } from './useTonClient';
 import { useAsyncInitialize } from './useAsyncInitialize';
+import { useTonConnect } from './useTonConnect';
 import { Address, OpenedContract } from '@ton/core';
 
 export function useCounterContract() {
   const client = useTonClient();
-  const [val, setVal] = useState<null | number>();
+  const [val, setVal] = useState<null | string>();
+  const { sender } = useTonConnect();
+
+  const sleep = (time: number) => new Promise((resolve) => setTimeout(resolve, time));
 
   const counterContract = useAsyncInitialize(async () => {
     if (!client) return;
     const contract = new Counter(
-      Address.parse('EQA7G0SKzziFlW5OgAD4Llnk4l0u1YnLFeB_Uz3yVO3eqU0-') // replace with your address from tutorial 2 step 8
+      Address.parse('EQBYLTm4nsvoqJRvs_L-IGNKwWs5RKe19HBK_lFadf19FUfb') // replace with your address from tutorial 2 step 8
     );
     return client.open(contract) as OpenedContract<Counter>;
   }, [client]);
@@ -21,7 +25,9 @@ export function useCounterContract() {
       if (!counterContract) return;
       setVal(null);
       const val = await counterContract.getCounter();
-      setVal(Number(val));
+      setVal(val.toString());
+      await sleep(5000); // sleep 5 seconds and poll value again
+      getValue();
     }
     getValue();
   }, [counterContract]);
@@ -29,5 +35,8 @@ export function useCounterContract() {
   return {
     value: val,
     address: counterContract?.address.toString(),
+    sendIncrement: () => {
+      return counterContract?.sendIncrement(sender);
+    },
   };
 }
